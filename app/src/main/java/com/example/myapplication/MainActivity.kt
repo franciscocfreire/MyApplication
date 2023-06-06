3b8febe5b56a6ee1b7e3cd1ec496a8dadc595f40
Francisco Freire <franciscocfreire@gmail.com>
2023-06-06 07:43:53 -0300

    Log Utilization

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("MYTAG", "OnCreated MainActivity")
         val greetingTextView = findViewById<TextView>(R.id.textView)
         val inputField = findViewById<EditText>(R.id.textName)
         val submitButton = findViewById<Button>(R.id.btnSubmit)
@@ -31,7 +33,9 @@ class MainActivity : AppCompatActivity() {
 
             } else {
                 val message = "Welcome $enteredName"
+                Log.i("MYTAG", "Message is $message")
                 greetingTextView.text = message
+                Log.i("MYTAG", "After displaying the message on the TextView")
                 inputField.text.clear()
                 offersButton.visibility = View.VISIBLE
 
@@ -45,5 +49,15 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MYTAG", "App is pause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MYTAG", "App is resume")
     }
 }
